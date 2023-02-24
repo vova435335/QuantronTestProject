ca993ce6d574d63d74fd4aa664d02404a39f0b85
Vladimir Krasnov <435335vova@gmail.com>
2023-02-24 18:26:06 +0300

    Установка русского языка для запроса

--- home/src/main/java/dev/vladimir/home/data/MovieApi.kt
+++ home/src/main/java/dev/vladimir/home/data/MovieApi.kt
@@ -2,9 +2,15 @@ package dev.vladimir.home.data
 
 import dev.vladimir.home.data.response.PopularMovieResponseModel
 import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val LANGUAGE_QUERY_KEY = "language"
+private const val DEFAULT_LANGUAGE = "ru-RU"
 
 interface MovieApi {
 
     @GET("movie/popular")
-    suspend fun getPopularMovies(): PopularMovieResponseModel
+    suspend fun getPopularMovies(
+        @Query(LANGUAGE_QUERY_KEY) language: String = DEFAULT_LANGUAGE
+    ): PopularMovieResponseModel
 }
