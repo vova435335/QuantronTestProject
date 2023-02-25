311fa2edb696c7e51fed0b265d5ae84e4d9b273c
Vladimir Krasnov <435335vova@gmail.com>
2023-02-25 04:08:47 +0300

    Обработка состояний

--- home/src/main/java/dev/vladimir/home/presintation/HomeFragment.kt
+++ home/src/main/java/dev/vladimir/home/presintation/HomeFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import dev.vladimir.core.data.common.observe
 import dev.vladimir.core.presentation.BaseFragment
+import dev.vladimir.core.presentation.adapters.DefaultBottomLoadStateAdapter
 import dev.vladimir.home.databinding.FragmentHomeBinding
+
+private const val SPAN_COUNT = 3
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
@@ -17,6 +22,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     private val viewModel by viewModels<HomeViewModel>()
 
     private lateinit var popularMovieAdapter: PopularMovieAdapter
+    private lateinit var popularMovieBottomAdapter: DefaultBottomLoadStateAdapter
 
     override fun createBinding(
         inflater: LayoutInflater,
@@ -27,6 +33,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         super.onViewCreated(view, savedInstanceState)
 
         initRecycler()
+        initSwipeRefresh()
         observeViewModel()
     }
 
@@ -37,11 +44,54 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     }
 
     private fun initRecycler() {
+        val layoutManager = GridLayoutManager(requireContext(), SPAN_COUNT)
+
         popularMovieAdapter = PopularMovieAdapter()
+        popularMovieBottomAdapter = DefaultBottomLoadStateAdapter { popularMovieAdapter.retry() }
 
         binding.homeRv.apply {
-            layoutManager = GridLayoutManager(requireContext(), 3)
-            adapter = popularMovieAdapter
+            this.layoutManager = layoutManager
+            adapter = popularMovieAdapter.withLoadStateFooter(popularMovieBottomAdapter)
+        }
+
+        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (position == popularMovieAdapter.itemCount
+                    && popularMovieBottomAdapter.itemCount > 0
+                ) {
+                    SPAN_COUNT
+                } else {
+                    1
+                }
+            }
+        }
+
+        listenLoadState()
+    }
+
+    private fun initSwipeRefresh() {
+        binding.homeSrl.setOnRefreshListener {
+            popularMovieAdapter.refresh()
+        }
+    }
+
+    private fun listenLoadState() {
+        popularMovieAdapter.addLoadStateListener {
+            with(binding) {
+                when (it.refresh) {
+                    is LoadState.Loading -> {
+                        homeSrl.isRefreshing = true
+                    }
+                    is LoadState.Error -> {
+                        homeSrl.isRefreshing = false
+                        homeError.root.isVisible = popularMovieAdapter.itemCount == 0
+                    }
+                    is LoadState.NotLoading -> {
+                        homeSrl.isRefreshing = false
+                        homeError.root.isVisible = popularMovieAdapter.itemCount == 0
+                    }
+                }
+            }
         }
     }
 }
